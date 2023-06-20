43a81f0314df2b6f8e50a98d0eb91b71f62d1761
Ranjita9535 <ranjitaxworkz@gmail.com>
2023-06-20 16:03:56 +0530

    test-push

---
+++ VariableSession.java
@@ -0,0 +1,16 @@
+public class VariableSession{
+public static void main(String[] args) {
+	int priceOfMobile = 100000;//initialiazing
+	System.out.println(priceOfMobile);
+	priceOfMobile=150000;//reassigning values
+	
+	System.out.println(priceOfMobile);
+}
+
+}
+
+---------------------
+
+int 
+byte
+string
