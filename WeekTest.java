e580b77b173e3e005891b615bbd938185bb72be8
Ranjita9535 <ranjitaxworkz@gmail.com>
2023-07-22 14:03:26 +0530

    test-push

---
+++ WeekTest.java
@@ -0,0 +1,71 @@
+// java Test 22/07/23
+
+
+1)11,34
+2) Zudio
+3) 50
+4) sum is 200       length is 4
+
+
+6) parameters are the datatypes which are specified inside the method braces. here we can pass the arguments or the values to the method parameter
+7) in the time of compilation only we get to know that which method we are using that is called as compile time polymorphisum. method overloding is also as compile time polymorphisum. 
+	example for compile time polymorphisum is method overloding
+	
+	
+	in method overloading there are 3 ways 
+	-> changing the number of parameter
+	-> changing the datatype of the parameter
+	-> alter the datatype of the parameter
+	
+8) 	Variable Arguments will takes a variable number of arguments. Variable Arguments simplifies the creation of methods that need to take a variable number of arguments.
+	Syntax of variable ars is
+	var 
+9) Package is group of similar types of clases,subclases ,and interfaces . package is always written in Reverse domain and package name is always in small letters.
+	package is keyword it is used create package in java
+ There are 2 types of package are present :
+ --> Built in package
+ ex: swing ,util
+ -->User-Defined Package
+ 
+ implicit imports: when we load all clasesof the package using * it will call as implicit package
+ explicit imports: clases are present inside the package,when we give proper path it will call as explicit imports
+
+10) Constructor Chaining :	calling constructor from one constructor to another constructor is called as Constructor Chaining.
+   
+   Constructor can be achive through using this() , super()
+   this()-> it calls constructor of the same class
+   super()-> it calls constructor of parent class
+   
+ 11) 
+ public class Fibonacci {
+
+    public static void main(String[] args) {
+        int n = 10; 
+        System.out.println("Fibonacci Sequence:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
+ 
+ 12) by using isListOdd(). by using the for loop we can check the odd numbers in java.
+ 
+ 13)
+ 
+ 14) Enumeration it is Special class consist of group of constants also it is called enum. enum is mainely used to restict the data.
+ 
+ 15)first the class will get loaded. after objects are created once class is completely loaded the jvm allocates the heap memory to th object 
+ 
+ 16)  Variables, methods, Constructor, datatypes ,loops
+ 
+ 
+     
+ 
